9e85019b75988d9dc0f96712a223a418ec2b140b
Prokudina Viktoriia <prokudina.viktoria@yandex.ru>
2026-07-07 12:47:28 +0500

    fix: "исправить замечания"

--- src/ru/yandex/practicum/exception/WordLengthException.java
+++ src/ru/yandex/practicum/exception/WordLengthException.java
@@ -3,5 +3,6 @@ package ru.yandex.practicum.exception;
 public class WordLengthException extends Exception {
 
     public WordLengthException() {
+
     }
 }
--- src/ru/yandex/practicum/exception/WordleDictionaryException.java
+++ src/ru/yandex/practicum/exception/WordleDictionaryException.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 public class WordleDictionaryException extends IOException {
 
-    public WordleDictionaryException(){
+    public WordleDictionaryException() {
     }
-
 }
